11305a701312f6a0afb6d10e435edd1774636012
MuBenYou <1458839724@qq.com>
2022-08-29 21:11:14 +0800

    8.29 任务保存

--- src/main/java/com/mby/WikiApplication.java
+++ src/main/java/com/mby/WikiApplication.java
@@ -9,15 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WikiApplication {
-//打印日志
+
     private  static final Logger LOG= LoggerFactory.getLogger(WikiApplication.class);
-
+    //LOG作用LoggerFactory.getLogger可以在IDE控制台打印日志,便于开发,一般加在代码最上面
     public static void main(String[] args) {
-
         SpringApplication app = new SpringApplication(WikiApplication.class);
+        //Environment 这个接口代表应用环境，它代表了两方面、一个是 profile 一个是 properties。
         Environment env = app.run(args).getEnvironment();
-        LOG.info("启动成功！！");
-        LOG.info("地址:\thttp://127.0.0.1:{}",env.getProperty("server.port"));//根据地址打印
+        LOG.info("启动成功！！");//LOg调用Info在控制台打印日志
+        LOG.info("地址:\thttp://127.0.0.1:{}",env.getProperty("server.port"));//获取到配置文件的参数端口号
 
 
     }
