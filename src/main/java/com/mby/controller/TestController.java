9802ed50125640b2cb66e39b1f7fd79d6360e23a
MuBenYou <1458839724@qq.com>
2022-08-30 15:52:47 +0800

    8.30 配置赋值

--- src/main/java/com/mby/controller/TestController.java
+++ src/main/java/com/mby/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.mby.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 //注解Controller返回一个界面
 @RestController//返回字符串数据
 public class TestController {
+    @Value("${test.hello:TEst}")
+    private String testHello;//获取配置的自定义的参数
     /*
     * GET获取，POST设置，PUT，DELETE删除
     * /user?id=1
@@ -21,7 +24,7 @@ public class TestController {
     @RequestMapping(value = "/user/1",method = RequestMethod.DELETE)*/
     @GetMapping("hello")//路径http://127.0.0.1:8080/hello
     public String hello(){
-        return  "Hello world";
+        return  "Hello world"+testHello;
     }
 
     @PostMapping("hello/post")//路径http://127.0.0.1:8080/hello
