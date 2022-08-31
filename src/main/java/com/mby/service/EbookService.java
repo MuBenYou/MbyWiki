9eb5e411674b949797f647426be13bb637ac60c3
MuBenYou <1458839724@qq.com>
2022-08-31 17:28:20 +0800

    8.31 封装请求参数和返回参数

--- src/main/java/com/mby/service/EbookService.java
+++ src/main/java/com/mby/service/EbookService.java
@@ -1,10 +1,15 @@
 package com.mby.service;
 
 import com.mby.domain.Ebook;
+import com.mby.domain.EbookExample;
 import com.mby.mapper.EbookMapper;
+import com.mby.req.EbookReq;
+import com.mby.resp.EbookResp;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service//注解 将这个Service交给Spring来管理
@@ -12,8 +17,19 @@ public class EbookService {
     @Resource
     private EbookMapper ebookMapper;
 
-    public List<Ebook> list() {
-        return  ebookMapper.selectByExample(null);//自动生成代码，selectByExample()查询所有字段
+    public List<EbookResp> list(EbookReq req) {
+        EbookExample ebookExample = new EbookExample();//domain下的example mybatis给我们自动生成了很多方法New 处理 才能调用他的方法
+        EbookExample.Criteria criteria=ebookExample.createCriteria();//Criteria相当于where条件， 把criteria创建出来
+        criteria.andNameLike("%"+req.getName()+"%");
+        List<Ebook> ebooklist=ebookMapper.selectByExample(ebookExample);//自动生成代码，selectByExample()查询所有字段,selectByExample=基本查询语句，括号里面是查询条件
+
+        List<EbookResp> respList =new ArrayList<>();
+        for (Ebook ebook : ebooklist){//
+            EbookResp ebookResp=new EbookResp();//把响应的类型new出来，才能使用
+            BeanUtils.copyProperties(ebook,ebookResp);
+            respList.add(ebookResp);
+        }
+        return  respList;
     }
 
 }
