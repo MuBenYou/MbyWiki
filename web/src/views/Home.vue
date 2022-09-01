c62f95960cba5e9990388c188badc78f174d8501
MuBenYou <1458839724@qq.com>
2022-09-01 15:46:03 +0800

    9.1 集成Ant Design Vue 及按钮

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -2,6 +2,10 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <div :style="{background:'rgb(235,200,125)',padding:'30px 20px 20px'}">
+      <a-button :size="size">MuBenYou</a-button>
+    </div>
+
   </div>
 </template>
 
