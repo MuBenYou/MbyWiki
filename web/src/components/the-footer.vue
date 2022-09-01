55a41af2a49afed16b68a773ebec6790fb34e2b5
MuBenYou <1458839724@qq.com>
2022-09-01 17:42:55 +0800

    9.1 制作header和footer组件

---
+++ web/src/components/the-footer.vue
@@ -0,0 +1,14 @@
+<template>
+  <a-layout-footer style="text-align: center">
+    MuBenYou 电子书 Ant Design ©2018 Created by Ant UED
+  </a-layout-footer>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'the-footer',
+
+});
+</script>
