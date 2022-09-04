b5d18eeaa183321311b917d38dfa7d170bf239d7
MuBenYou <1458839724@qq.com>
2022-09-04 14:56:28 +0800

    9.4 修改电子书管理页面

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -1,5 +1,22 @@
 <template>
-  <div class="admin-ebook">
-    <h1>电子书</h1>
-  </div>
+  <a-layout>
+    <a-layout-content
+        :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+    >
+      <div class="about">
+        <h1>带你指数</h1>
+      </div>
+
+    </a-layout-content>
+  </a-layout>
+
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import TheHeader from '@/components/the-header.vue';
+import TheFooter from '@/components/the-footer.vue';
+export default defineComponent({
+  name: 'damin-ebook'
+});
+</script>
